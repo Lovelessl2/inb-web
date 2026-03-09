f5837667cd6459db6e0bb597a2931ec3ee65a644
Lovelessl2 <skalant.contenido@gmail.com>
2026-03-09 13:49:05 -0600

    Delete INB Web directory

--- INB Web/js/script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.querySelector(".menu_toggle");
-const menu = document.querySelector(".menu_lateral");
-
-toggle.addEventListener("click", () => {
-    menu.classList.toggle("active");
-});
